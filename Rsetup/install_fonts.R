f98034fd498b5dfde4fade71c044053a2076ea24
sheffien <sheffien@users.noreply.github.com>
2015-05-05 15:31:41 +0200

    Don't reinstall arial if already installed

--- Rsetup/install_fonts.R
+++ Rsetup/install_fonts.R
@@ -4,16 +4,21 @@
 if(require("extrafont")) {
 	if ("Arial" %in% fonts()) {
 		message("Arial already installed.")
+		skip=TRUE;
 	}
 }
 
-local({r <- getOption("repos");
-       r["CRAN"] <- "http://cran.at.r-project.org";  #AUSTRIA mirror
-       options(repos=r)})
+# Otherwise, install it!
+if (! skip ) {
+	local({r <- getOption("repos");
+		   r["CRAN"] <- "http://cran.at.r-project.org";  #AUSTRIA mirror
+		   options(repos=r)})
 
-install.packages(c("extrafont"))
-library("extrafont")
+	install.packages(c("extrafont"))
+	library("extrafont")
 
-# importing takes awhile but only has to be done once.
-font_import("Rsetup/fonts/", prompt=FALSE)
-loadfonts()
+	# importing takes awhile but only has to be done once.
+	font_import("Rsetup/fonts/", prompt=FALSE)
+	loadfonts()
+}
+
