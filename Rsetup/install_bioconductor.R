dbe5a5bc2866a0b4ead25398cd63ea06bc6f3970
Nathan Sheffield <nsheff@users.noreply.github.com>
2016-08-03 17:52:14 -0700

    Update install_bioconductor.R

--- Rsetup/install_bioconductor.R
+++ Rsetup/install_bioconductor.R
@@ -1,3 +1,5 @@
 #! Rscript
-source("http://bioconductor.statistik.tu-dortmund.de/biocLite.R")
+#source("http://bioconductor.statistik.tu-dortmund.de/biocLite.R")
+source("https://bioconductor.org/biocLite.R")
 biocLite(suppressUpdates=TRUE)
+        
